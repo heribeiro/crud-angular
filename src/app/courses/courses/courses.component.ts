9a3628aec548b0fceab98998ee3bb095cff283fd
heribeiro <helena.correa84@gmail.com>
2023-10-23 17:37:58 -0300

    criação do assets Json e importação do HttpCLient

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -8,16 +10,19 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: "1", name: 'Angular', category: 'front-end'},
-    { _id: "2", name: 'React', category: 'front-end'}
-  ];
+  courses: Observable <Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() {
+ // coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
+  //  this.courses =[];
+ //   this.coursesService = new CoursesService();
+      this.courses = this.coursesService.list();
     }
 
   ngOnInit(): void {
+
 
   }
 
